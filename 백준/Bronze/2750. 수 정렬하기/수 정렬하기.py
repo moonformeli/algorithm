f065ed89ed364fd4a31701a2fe1ec78bd0dfd75b
moonformeli <mgyang95@gmail.com>
2024-07-12 17:28:31 +0900

    [Bronze II] Title: 수 정렬하기, Time: 104 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -6,13 +6,16 @@ nums = []
 for _ in range(N):
   nums.append(int(sys.stdin.readline().strip()))
 
-# 버블 정렬
-for i in range(len(nums)):
-  for j in range(len(nums) - i - 1):
-    if nums[j] > nums[j + 1]:
-      tmp = nums[j + 1]
-      nums[j + 1] = nums[j]
-      nums[j] = tmp
-
+# 삽입 정렬
+for i in range(1, len(nums)):
+  if nums[i] < nums[i - 1]:
+    for j in range(i, 0, -1):
+      if nums[j] < nums[j - 1]:
+        tmp = nums[j - 1]
+        nums[j - 1] = nums[j]
+        nums[j] = tmp
+      else:
+        break
+  
 for num in nums:
   print(num)
